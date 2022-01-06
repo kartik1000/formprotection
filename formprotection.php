0ff8b0476c46ae4f50632e6423c027bc52e1999f
Kartik Kathuria <kathuriakartik0@gmail.com>
2022-01-07 03:14:07 +0530

    Update formprotection.php

--- formprotection.php
+++ formprotection.php
@@ -4,7 +4,6 @@ require_once 'formprotection.civix.php';
 // phpcs:disable
 use CRM_Formprotection_ExtensionUtil as E;
 // phpcs:enable
-
 /**
  * Implements hook_civicrm_config().
  *
@@ -12,15 +11,6 @@ use CRM_Formprotection_ExtensionUtil as E;
  */
 function formprotection_civicrm_config(&$config) {
   _formprotection_civix_civicrm_config($config);
-}
-
-/**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function formprotection_civicrm_xmlMenu(&$files) {
-  _formprotection_civix_civicrm_xmlMenu($files);
 }
 
 /**
@@ -64,7 +54,7 @@ function formprotection_civicrm_enable() {
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
  */
-function formprotection_civicrm_disable() {
+function formprotectioncivicrm_disable() {
   _formprotection_civix_civicrm_disable();
 }
 
@@ -75,54 +65,6 @@ function formprotection_civicrm_disable() {
  */
 function formprotection_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   return _formprotection_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function formprotection_civicrm_managed(&$entities) {
-  _formprotection_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function formprotection_civicrm_caseTypes(&$caseTypes) {
-  _formprotection_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function formprotection_civicrm_angularModules(&$angularModules) {
-  _formprotection_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function formprotection_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _formprotection_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -137,29 +79,11 @@ function formprotection_civicrm_entityTypes(&$entityTypes) {
 }
 
 /**
- * Implements hook_civicrm_themes().
- */
-function formprotection_civicrm_themes(&$themes) {
-  _formprotection_civix_civicrm_themes($themes);
-}
-
-// --- Functions below this ship commented out. Uncomment as required. ---
-
-/**
- * Implements hook_civicrm_preProcess().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
- */
-//function formprotection_civicrm_preProcess($formName, &$form) {
-//
-//}
-
-/**
  * Implements hook_civicrm_navigationMenu().
  */
 function formprotection_civicrm_navigationMenu(&$menu) {
   _formprotection_civix_insert_navigation_menu($menu, 'Administer/System Settings', [
-    'label' => E::ts('reCAPTCHA v3 Settings'),
+    'label' => E::ts('reCAPTCHA Settings'),
     'name' => 'recaptcha_settings',
     'url' => 'civicrm/admin/setting/recaptcha',
     'permission' => 'administer CiviCRM',
@@ -175,6 +99,9 @@ function formprotection_civicrm_navigationMenu(&$menu) {
 function formprotection_civicrm_buildForm($formName, &$form) {
   switch ($formName) {
     case 'CRM_Admin_Form_Generic':
+      if ($form->getSettingPageFilter() !== 'recaptcha') {
+        return;
+      }
 
       $helpText = E::ts(
           'reCAPTCHA is a free service that helps prevent automated abuse of your site. To use it on public-facing CiviCRM forms: sign up at <a href="%1" target="_blank">Google\'s reCaptcha site</a>; enter the provided public and private keys here; then enable reCAPTCHA under Advanced Settings in any Profile.',
@@ -182,11 +109,25 @@ function formprotection_civicrm_buildForm($formName, &$form) {
             1 => 'https://www.google.com/recaptcha',
           ]
         )
-        . '<br/><strong>' . E::ts('Only the reCAPTCHA v3 is supported.') . '</strong>';
+        . '<br/><strong>' . E::ts('Only the reCAPTCHA v2 checkbox type is supported.') . '</strong>';
       \Civi::resources()
         ->addMarkup('<div class="help">' . $helpText . '</div>', [
           'weight' => -1,
           'region' => 'page-body',
         ]);
   }
+}
+
+function formprotection_civicrm_validateForm($formName, &$fields, &$files, &$form, &$errors) {
+	require_once E::path('lib/recaptcha/recaptchalib.php');
+	echo '<script>alert(' . $_POST['g-recaptcha-token'] . ')</script>';
+	print_r($fields);
+	$resp = recaptcha_check_answer(CRM_Core_Config::singleton()->recaptchaPrivateKey,
+      $_SERVER['REMOTE_ADDR'],
+      $_POST['g-recaptcha-token']
+    );
+	//$resp->is_valid = FALSE;
+	if(!$resp->is_valid) {
+		$errors['recaptcha'] = E::ts('ReCAPTCHA v3 token not generated or invalid score');
+	}
 }
