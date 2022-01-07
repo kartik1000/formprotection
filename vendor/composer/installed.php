d8176344c3ef89f69de27ce8772bb87c9c892cb1
Matthew Wire <mjw@mjwconsult.co.uk>
2022-01-07 10:25:13 +0000

    Add Google ReCAPTCHA composer library

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0ff8b0476c46ae4f50632e6423c027bc52e1999f',
+    'name' => 'civicrm/formprotection',
+  ),
+  'versions' => 
+  array (
+    'civicrm/formprotection' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0ff8b0476c46ae4f50632e6423c027bc52e1999f',
+    ),
+    'google/recaptcha' => 
+    array (
+      'pretty_version' => '1.2.4',
+      'version' => '1.2.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '614f25a9038be4f3f2da7cbfd778dc5b357d2419',
+    ),
+  ),
+);
